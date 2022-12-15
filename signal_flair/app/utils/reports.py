5de3cb5866a37aa23b60649893a9b560cd9289b3
Zain Raza <sraza@ptc.com>
2022-12-15 09:42:19 -0500

    docstring + form default value update

--- signal_flair/app/utils/reports.py
+++ signal_flair/app/utils/reports.py
@@ -5,12 +5,25 @@ import matplotlib
 
 
 def get_save_path():
-    """ """
+    """Used to list/display past reports."""
     return dirname(dirname(abspath(__file__))) + "/views/reports/"
 
 
 def generate_pandas_prof_report(df, title):
-    """ """
+    """
+    Generate a new report, and save to disk.
+
+    Parameters:
+        df(pd.DataFrame): data parsed from the user-uploaded file
+        report_config(dict): assumed to contain 3 key-value pairs:
+            1) "report-title" (str) - should be submitted through the
+            form on the "/load-file" route
+            2) "column-to-analyze" - if doing outlier detection, this is
+            expected to be the case-sensitive name of one of the numerical
+            columns in the uploaded CSV
+
+    Returns: str: the title of the report (signals that execution was successful)
+    """
     matplotlib.use("agg")
     profile = ProfileReport(df, title=title, explorative=False)
 
