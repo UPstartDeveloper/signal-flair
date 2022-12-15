a30bec0d693531c81937e7d7e2540bd488a587e6
Zain Raza <sraza@ptc.com>
2022-12-15 09:52:27 -0500

    debug TypeError, use report title on form submission

--- signal_flair/app/utils/reports.py
+++ signal_flair/app/utils/reports.py
@@ -1,7 +1,32 @@
-from pandas_profiling import ProfileReport
 from os.path import dirname, abspath
+import locale
 
 import matplotlib
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+# allows us to parse English strings representing numbers
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8' )
+# non-interactive backend, so we don't crash the browser
+matplotlib.use("agg")
+
+
+def parse_report_title(title: str, fpath: str) -> str:
+    """
+    In case user has not titled their report, 
+    provide one based on the name of their
+    provided CSV module.
+    """
+    fpath, title = fpath.replace("\\", "/"), ""
+    input_title = title
+    if input_title:
+        title = title["report-title"]
+    else:
+        title = fpath.split("/")[-1].split(".")[-2]
+
+    return title
 
 
 def get_save_path():
@@ -9,7 +34,9 @@ def get_save_path():
     return dirname(dirname(abspath(__file__))) + "/views/reports/"
 
 
-def generate_pandas_prof_report(df, title):
+def generate_pandas_prof_report(
+    df: pd.DataFrame, report_config: dict, fpath: str
+) -> str:
     """
     Generate a new report, and save to disk.
 
@@ -24,10 +51,23 @@ def generate_pandas_prof_report(df, title):
 
     Returns: str: the title of the report (signals that execution was successful)
     """
-    matplotlib.use("agg")
-    profile = ProfileReport(df, title=title, explorative=False)
+    # unpack the params needed to configure the report
+    title = parse_report_title(report_config["report-title"], fpath)
 
+    column_to_analyze = report_config["column-to-analyze"]
+    # prevent errors in parsing number strs with commas
+    if df[column_to_analyze].dtype == np.dtype("object"):
+        df = df.assign(
+            column_to_analyze=df[column_to_analyze]\
+                .apply(lambda duration_str: locale.atoi(duration_str))
+        )
+
+    # make the report
     output_path = get_save_path()
     output_path = output_path + title + ".html"
 
-    profile.to_file(output_path)
+    # regular exploratory data stats + analysis
+    eda_profile = ProfileReport(df, title=title, explorative=True)
+    eda_profile.to_file(output_path)
+
+    return title
